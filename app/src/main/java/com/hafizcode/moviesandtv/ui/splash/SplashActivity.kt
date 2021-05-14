1f4054658c563f337efe39e9e4189917408a0620
HafiCode02 <hafizcode02@gmail.com>
2021-05-14 20:02:58 +0700

    setting splash screen example

--- app/src/main/java/com/hafizcode/moviesandtv/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/hafizcode/moviesandtv/ui/splash/SplashActivity.kt
@@ -1,12 +1,24 @@
 package com.hafizcode.moviesandtv.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.hafizcode.moviesandtv.R
+import com.hafizcode.moviesandtv.ui.home.view.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
