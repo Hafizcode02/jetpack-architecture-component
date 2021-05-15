0eba8cac19486b8cb0559b9fbdff6792e9f92f2e
HafiCode02 <hafizcode02@gmail.com>
2021-05-15 16:58:17 +0700

    setting up slider

--- app/src/main/java/com/hafizcode/moviesandtv/ui/home/view/HomeActivity.kt
+++ app/src/main/java/com/hafizcode/moviesandtv/ui/home/view/HomeActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hafizcode.moviesandtv.R
 import com.hafizcode.moviesandtv.databinding.ActivityHomeBinding
+import com.synnapps.carouselview.ImageListener
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,19 @@ class HomeActivity : AppCompatActivity() {
         val sectionPagerAdapter = SectionPagerAdapter(this, supportFragmentManager)
         activityHomeBinding.viewPager.adapter = sectionPagerAdapter
         activityHomeBinding.tabs.setupWithViewPager(activityHomeBinding.viewPager)
+
+        val carouselImages = intArrayOf(
+            R.drawable.slider_avengers,
+            R.drawable.slider_naruto,
+            R.drawable.slider_ipman,
+            R.drawable.slider_ncis
+        )
+
+        val imageListener = ImageListener { position, imageView ->
+            imageView.setImageResource(carouselImages[position])
+        }
+
+        activityHomeBinding.carouselView.pageCount = carouselImages.size
+        activityHomeBinding.carouselView.setImageListener(imageListener)
     }
 }
